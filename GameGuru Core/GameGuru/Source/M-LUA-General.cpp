1f99603dcc2c9d0fea17ce39239cbf4372ba6d76
HierToo <17246996+HierToo@users.noreply.github.com>
2018-05-04 01:02:07 +0200

    Fix Bug -  SetSkyTo() does not show last sky in folder

--- GameGuru Core/GameGuru/Source/M-LUA-General.cpp
+++ GameGuru Core/GameGuru/Source/M-LUA-General.cpp
@@ -1168,7 +1168,7 @@ void lua_setloadingresource ( void )
 void lua_set_sky(void)
 {
 	int index = -1;
-	for (int i = 0; i < ArrayCount(t.skybank_s); i++) 
+	for (int i = 0; i <= ArrayCount(t.skybank_s); i++) 
 	{
 		if (t.skybank_s[i].Lower()==t.s_s.Lower()) 
 		{
