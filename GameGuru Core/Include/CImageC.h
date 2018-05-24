15d09d9f0c211f176da69bcaa51f90c3c369ff49
Preben Eriksen <plemsoft@plemsoft.com>
2018-05-24 15:02:36 +0200

    PE-MEM: Added new functions definations and AccessCountGPU/CPU to allow us to record what textures is actually used.

--- GameGuru Core/Include/CImageC.h
+++ GameGuru Core/Include/CImageC.h
@@ -32,6 +32,9 @@ struct tagImgData
 	int						iDepth;						// depth of texture
 	bool					bLocked;					// is locked
 
+	int						AccessCountGPU;				// PE: Number of times sent to GPU
+	int						AccessCountCPU;				// PE: Number of times CPU accessed the image.
+
 	float					fTexUMax;					// U-coord for texture clip
 	float					fTexVMax;					// V-coord for texture clip
 
@@ -48,6 +51,7 @@ DARKSDK bool                UpdateImagePtr				( int iID );
 DARKSDK LPGGTEXTURE			GetTexture					( char* szFilename, GGIMAGE_INFO* info, int iOneToOnePixels );
 
 DARKSDK void				ImageConstructor			( void );
+DARKSDK void				DumpImageList				( void ); //PE: Image access debug.
 DARKSDK void				ImageDestructor				( void );
 DARKSDK void				ImageSetErrorHandler		( LPVOID pErrorHandlerPtr );
 DARKSDK void				PassSpriteInstance			( void );
@@ -59,6 +63,7 @@ DARKSDK void				StretchImage				( int iID, int iSizeX, int iSizeY );
 DARKSDK int					LoadImageInternal				( char* szFilename );
 DARKSDK int					LoadImageInternalEx				( char* szFilename, int iDivideTextureSize );
 DARKSDK void				ClearAnyLightMapInternalTextures ( void );
+DARKSDK void				ClearAnyEntitybankInternalTextures(void);
 
 DARKSDK LPGGTEXTURE			MakeImage						( int iID, int iWidth, int iHeight );	// makes a new image
 DARKSDK LPGGTEXTURE			MakeImageUsage					( int iID, int iWidth, int iHeight, DWORD dwUsage );
