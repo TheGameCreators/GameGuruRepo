d06e75a7d03a21868041ed4290eafbe915297335
AmenMoses <chrisstapleton@ntlworld.com>
2018-09-28 21:25:36 +0100

    Re-write of the particle system exposing the workings via Lua commands for full script control.

--- GameGuru Core/GameGuru/Include/M-Particles.h
+++ GameGuru Core/GameGuru/Include/M-Particles.h
@@ -4,13 +4,22 @@
 
 #include "cstr.h"
 
-void ravey_particles_init ( void );
-void ravey_particles_load_images ( void );
-void ravey_particles_update ( void );
-void ravey_particles_update_emitters ( void );
-void ravey_particles_update_particles ( void );
-void ravey_particles_free ( void );
-void ravey_particles_add_emitter ( void );
-void ravey_particles_delete_emitter ( void );
-void ravey_particles_delete_all_emitters ( void );
-void ravey_particles_get_free_emitter ( void );
+void ravey_particles_init(void);
+void ravey_particles_load_images(void);
+int  ravey_particles_load_image(LPSTR szFilename, int iID);
+void ravey_particles_update(void);
+void ravey_particles_update_emitters(void);
+bool ravey_particles_update_particles(void);
+void ravey_particles_free(void);
+void ravey_particles_add_emitter(void);
+void ravey_particles_delete_emitter(void);
+void ravey_particles_delete_all_emitters(void);
+void ravey_particles_get_free_emitter(void);
+void ravey_particles_set_frames(int iID, int iAnimSpeed, int iStartFrame, int iEndFrame);
+void ravey_particles_set_speed(int iID, float fMinX, float fMinY, float fMinZ, float fMaxX, float fMaxY, float fMaxZ);
+void ravey_particles_set_offset(int iID, float fMinX, float fMinY, float fMinZ, float fMaxX, float fMaxY, float fMaxZ);
+void ravey_particles_set_rotate(int iID, float fMinXRot, float fMaxXRot, float fMinYRot, float fMaxYRot, float fMinZRot, float fMaxZRot, int bStartsOffRandom);
+void ravey_particles_set_scale(int iID, float fStartMin, float fStartMax, float fEndMin, float fEndMax);
+void ravey_particles_set_alpha(int iID, float fStartMin, float fStartMax, float fEndMin, float fEndMax);
+void ravey_particles_set_life(int iID, int iSpawnRate, float iLifeMin, float iLifeMax, int iMaxParticles, int iOnDeathAction);
+void ravey_particles_set_wind_vector(float fwindX, float fwindZ);
