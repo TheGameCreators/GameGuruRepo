f353f3b96e687b4f3b519807c3361ec77a0b2835
Lee Bamber <35840583+LeeBamberTGC@users.noreply.github.com>
2022-07-05 15:12:55 +0100

    Cleanup Code

--- GameGuru Core/GameGuru/Include/Common-Keys.h
+++ GameGuru Core/GameGuru/Include/Common-Keys.h
@@ -1,5 +1,11 @@
+// Defines
 #define DISCOUNTKEY "sc=[discountcodekey]"
 #define FPMHOSTUPLOADKEY "sc=[hostuploadkey]"
 #define DOWNLOADSTOREKEY "sc=[none]"
 #define NEWSTOREKEY "[none]"
 #define NEWSTOREPASSWORD "[none]"
+
+// Functions
+void common_makeserialcode (LPSTR pInstituteName, int iFromDD, int iFromMM, int iFromYY, int iToDD, int iToMM, int iToYY, LPSTR pBuildString);
+int common_isserialcodevalid (LPSTR pSerialCode, LPSTR pOptionalUserName);
+void common_writeserialcode (LPSTR pCode, LPSTR pOptionalUsername);
---
+++ GameGuru Core/GameGuru/Source/Common-Keys.cpp
@@ -0,0 +1,23 @@
+// Includes
+#include "stdafx.h"
+#include "gameguru.h"
+#include "time.h"
+#include "direct.h"
+#include <wininet.h>
+
+// Functions
+void common_makeserialcode (LPSTR pInstituteName, int iFromDD, int iFromMM, int iFromYY, int iToDD, int iToMM, int iToYY, LPSTR pBuildString)
+{
+	// hello world
+}
+
+int common_isserialcodevalid (LPSTR pSerialCode, LPSTR pOptionalUserName)
+{
+	// hello world
+	return 1;
+}
+
+void common_writeserialcode (LPSTR pCode, LPSTR pOptionalUsername)
+{
+	// hello world
+}
