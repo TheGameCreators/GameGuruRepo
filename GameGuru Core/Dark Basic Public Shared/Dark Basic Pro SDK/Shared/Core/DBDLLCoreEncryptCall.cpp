c99d18177bd028de2741ecd497d2e851f7d97541
Lee Bamber <35840583+LeeBamberTGC@users.noreply.github.com>
2023-05-14 23:48:01 +0100

    Tweak
    * Clean up commit

--- GameGuru Core/Dark Basic Public Shared/Dark Basic Pro SDK/Shared/Core/DBDLLCoreEncryptCall.cpp
+++ GameGuru Core/Dark Basic Public Shared/Dark Basic Pro SDK/Shared/Core/DBDLLCoreEncryptCall.cpp
@@ -1,7 +1,11 @@
 #include "windows.h"
 #include "globstruct.h"
 #include "DBDLLCore.h"
+#ifdef WICKEDENGINE
+#include "..\..\..\..\Guru-WickedMAX\Encryptor.h"
+#else
 #include "..\..\..\..\Guru-MapEditor\Encryptor.h"
+#endif
 #include ".\..\Core\SteamCheckForWorkshop.h"
 
 void CallEncryptDecrypt( char* dwStringAddress, bool bEncryptIfTrue, bool bDoNotUseTempFolder )
