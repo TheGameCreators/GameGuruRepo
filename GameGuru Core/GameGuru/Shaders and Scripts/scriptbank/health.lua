f9a9223107d855fb1ceb87ce98980ab835b6e872
Teabone <35990109+Teabone3@users.noreply.github.com>
2023-06-29 01:27:59 -0400

    Update health.lua
    This update will prevent the health from exceeding the maximum amount and act more as "health recovery" for items such as med-kits and alike.
    
    Additionally, it will display a message if your health is already full.

--- GameGuru Core/GameGuru/Shaders and Scripts/scriptbank/health.lua
+++ GameGuru Core/GameGuru/Shaders and Scripts/scriptbank/health.lua
@@ -1,16 +1,17 @@
 -- LUA Script - precede every function and global member with lowercase name of script + '_main'
 -- Player Collects Health
 
-function health_init(e)
-end
-
 function health_main(e)
  PlayerDist = GetPlayerDistance(e)
- if PlayerDist < 80 and g_PlayerHealth > 0 then
-   Prompt("Collected health")
-   PlaySound(e,0)
-   AddPlayerHealth(e)
-   Destroy(e)
-   ActivateIfUsed(e)
+ if PlayerDist < 80 then
+	if g_PlayerHealth > 0 and g_PlayerHealth < g_gameloop_StartHealth then
+	Prompt("Collected "..item_name)
+	PlaySound(e,0)
+	AddPlayerHealth(e)
+	Destroy(e)
+	ActivateIfUsed(e)
+	else
+	Prompt("Your health is full.")
+	end
  end
 end
