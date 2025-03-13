0f6717de7142def47d73ddef7f052474b868a341
Teabone <35990109+Teabone3@users.noreply.github.com>
2025-03-13 01:58:14 -0400

    Update ammo_highlight.lua (3rd person ammo)
    Bypasses 3rd person player check to allow for collection of ammo in 3rd person.

--- GameGuru Core/GameGuru/Shaders and Scripts/scriptbank/ammo_highlight.lua
+++ GameGuru Core/GameGuru/Shaders and Scripts/scriptbank/ammo_highlight.lua
@@ -7,10 +7,10 @@ end
 
 function ammo_highlight_main(e)
  PlayerDist = GetPlayerDistance(e)
- if PlayerDist < plr_highlight_ammo_range and g_PlayerHealth > 0 then
+ if PlayerDist < plr_highlight_ammo_range then
 	SetEntityHighlight(e,1) --1(red),2(pink),3(green),4(blue/green),5(gold)
  end
- if PlayerDist < 60 and g_PlayerHealth > 0 and g_PlayerThirdPerson==0 then
+ if PlayerDist < 60 and g_PlayerHealth > 0 then
    Prompt("Collected ammo")
    PlaySound(e,0)
    AddPlayerAmmo(e)
